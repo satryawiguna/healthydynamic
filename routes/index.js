3cebf5710c409deee55cd81e4fb245c357b98f50
Satrya Wiguna <satrya@freshcms.net>
2022-02-21 21:11:16 +0800

    HD-04_add_middleware_function

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getUsers, register, login } from "../controllers/UserController.js";
+import { getUsers, getUser, register, login } from "../controllers/UserController.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
 
 const router = express.Router();
 
-router.get("/users", getUsers);
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/users", verifyToken, getUsers);
+router.get("/user/:id", verifyToken, getUser);
 
 export default router;
